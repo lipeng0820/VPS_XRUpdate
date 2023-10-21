3ef1436db60bfaed319f7d97501549f181c70b82
lipeng0820 <lipeng0820@qq.com>
2023-10-21 23:22:59 +0800

    Update XRUpdate.sh

--- XRUpdate.sh
+++ XRUpdate.sh
@@ -23,9 +23,11 @@ certMode=$(yq e '.Nodes[0].ControllerConfig.CertConfig.CertMode' ~/config.yml.ba
 certDomain=$(yq e '.Nodes[0].ControllerConfig.CertConfig.CertDomain' ~/config.yml.bak)
 provider=$(yq e '.Nodes[0].ControllerConfig.CertConfig.Provider' ~/config.yml.bak)
 email=$(yq e '.Nodes[0].ControllerConfig.CertConfig.Email' ~/config.yml.bak)
-dnsEnv=$(yq e '.Nodes[0].ControllerConfig.CertConfig.DNSEnv' ~/config.yml.bak)
 
-# 更新config.yml.example文件
+# 提取DNSEnv为一个临时文件
+yq e '.Nodes[0].ControllerConfig.CertConfig.DNSEnv' ~/config.yml.bak > ~/temp_dns_env.yml
+
+# 更新config.yml.example文件的其他参数
 yq eval -i ".Nodes[0].PanelType = \"$panelType\"" ~/config.yml.example
 yq eval -i ".Nodes[0].ApiConfig.ApiHost = \"$apiHost\"" ~/config.yml.example
 yq eval -i ".Nodes[0].ApiConfig.ApiKey = \"$apiKey\"" ~/config.yml.example
@@ -35,7 +37,12 @@ yq eval -i ".Nodes[0].ControllerConfig.CertConfig.CertMode = \"$certMode\"" ~/co
 yq eval -i ".Nodes[0].ControllerConfig.CertConfig.CertDomain = \"$certDomain\"" ~/config.yml.example
 yq eval -i ".Nodes[0].ControllerConfig.CertConfig.Provider = \"$provider\"" ~/config.yml.example
 yq eval -i ".Nodes[0].ControllerConfig.CertConfig.Email = \"$email\"" ~/config.yml.example
-yq eval -i ".Nodes[0].ControllerConfig.CertConfig.DNSEnv = $dnsEnv" ~/config.yml.example
+
+# 更新config.yml.example文件的DNSEnv部分
+yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' ~/config.yml.example ~/temp_dns_env.yml > ~/temp_updated.yml
+
+# 移动临时更新文件以替换config.yml.example
+mv ~/temp_updated.yml ~/config.yml.example
 
 # 重命名config.yml.example为config.yml
 mv ~/config.yml.example ~/config.yml
@@ -45,3 +52,6 @@ cat <<EOL > ~/XrayrConfigUpdate.log
 旧值 ---> 新值
 ApiHost: "$apiHost" ---> ApiHost: "$(yq e '.Nodes[0].ApiConfig.ApiHost' ~/config.yml)"
 EOL
+
+# 清理临时文件
+rm ~/temp_dns_env.yml
