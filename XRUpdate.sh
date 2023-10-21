e3bbeb3de44486deb669ecea1945abff598d15dc
lipeng0820 <lipeng0820@qq.com>
2023-10-21 23:37:18 +0800

    Update XRUpdate.sh

--- XRUpdate.sh
+++ XRUpdate.sh
@@ -1,63 +1,52 @@
 #!/bin/bash
 
-# 检查yq是否已安装
+# 安装 yq 工具
 if ! command -v yq &> /dev/null
 then
-    # 如果yq未安装，则下载并安装yq
-    wget https://github.com/mikefarah/yq/releases/download/v4.13.5/yq_linux_amd64 -O /usr/bin/yq && chmod +x /usr/bin/yq
+    echo "yq could not be found, installing now..."
+    wget -O /usr/bin/yq https://github.com/mikefarah/yq/releases/download/v4.13.5/yq_linux_amd64
+    chmod +x /usr/bin/yq
 fi
 
-# 下载最新的config.yml.example文件
-wget -O ~/config.yml.example https://raw.githubusercontent.com/XrayR-project/XrayR/master/release/config/config.yml.example
-
-# 检查config.yml文件是否存在
+# 检查 config.yml 是否存在
 if [[ ! -f ~/config.yml ]]; then
     echo "Error: config.yml file not found!"
     exit 1
 fi
 
-# 备份旧的config.yml文件
+# 下载新的 config.yml.example
+wget -O ~/config.yml.example https://raw.githubusercontent.com/XrayR-project/XrayR/master/release/config/config.yml.example
+
+# 备份当前的 config.yml
 mv ~/config.yml ~/config.yml.bak
 
-# 提取用户配置文件（config.yml.bak）中的参数
-panelType=$(yq e '.Nodes[0].PanelType' ~/config.yml.bak)
-apiHost=$(yq e '.Nodes[0].ApiConfig.ApiHost' ~/config.yml.bak)
-apiKey=$(yq e '.Nodes[0].ApiConfig.ApiKey' ~/config.yml.bak)
-nodeID=$(yq e '.Nodes[0].ApiConfig.NodeID' ~/config.yml.bak)
-nodeType=$(yq e '.Nodes[0].ApiConfig.NodeType' ~/config.yml.bak)
-certMode=$(yq e '.Nodes[0].ControllerConfig.CertConfig.CertMode' ~/config.yml.bak)
-certDomain=$(yq e '.Nodes[0].ControllerConfig.CertConfig.CertDomain' ~/config.yml.bak)
-provider=$(yq e '.Nodes[0].ControllerConfig.CertConfig.Provider' ~/config.yml.bak)
-email=$(yq e '.Nodes[0].ControllerConfig.CertConfig.Email' ~/config.yml.bak)
+# 提取所需的参数值并将它们复制到 config.yml.example
+for param in PanelType ApiHost ApiKey NodeID NodeType CertMode CertDomain Provider Email; do
+    value=$(yq e ".Nodes[0].${param}" ~/config.yml.bak)
+    yq eval -i ".Nodes[0].${param} = ${value}" ~/config.yml.example
+done
 
-# 提取DNSEnv为一个临时文件
-yq e '.Nodes[0].ControllerConfig.CertConfig.DNSEnv' ~/config.yml.bak > ~/temp_dns_env.yml
+# 替换 DNSEnv 下的两个参数
+dnsEnv=$(yq e ".Nodes[0].ControllerConfig.CertConfig.DNSEnv" ~/config.yml.bak)
+yq eval -i ".Nodes[0].ControllerConfig.CertConfig.DNSEnv = ${dnsEnv}" ~/config.yml.example
 
-# 更新config.yml.example文件的其他参数
-yq eval -i ".Nodes[0].PanelType = \"$panelType\"" ~/config.yml.example
-yq eval -i ".Nodes[0].ApiConfig.ApiHost = \"$apiHost\"" ~/config.yml.example
-yq eval -i ".Nodes[0].ApiConfig.ApiKey = \"$apiKey\"" ~/config.yml.example
-yq eval -i ".Nodes[0].ApiConfig.NodeID = $nodeID" ~/config.yml.example
-yq eval -i ".Nodes[0].ApiConfig.NodeType = \"$nodeType\"" ~/config.yml.example
-yq eval -i ".Nodes[0].ControllerConfig.CertConfig.CertMode = \"$certMode\"" ~/config.yml.example
-yq eval -i ".Nodes[0].ControllerConfig.CertConfig.CertDomain = \"$certDomain\"" ~/config.yml.example
-yq eval -i ".Nodes[0].ControllerConfig.CertConfig.Provider = \"$provider\"" ~/config.yml.example
-yq eval -i ".Nodes[0].ControllerConfig.CertConfig.Email = \"$email\"" ~/config.yml.example
+# 删除 ALICLOUD_ACCESS_KEY 和 ALICLOUD_SECRET_KEY
+yq eval -i "del(.Nodes[0].ControllerConfig.CertConfig.DNSEnv.ALICLOUD_ACCESS_KEY)" ~/config.yml.example
+yq eval -i "del(.Nodes[0].ControllerConfig.CertConfig.DNSEnv.ALICLOUD_SECRET_KEY)" ~/config.yml.example
 
-# 更新config.yml.example文件的DNSEnv部分
-yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' ~/config.yml.example ~/temp_dns_env.yml > ~/temp_updated.yml
+# 将 CLOUDFLARE_EMAIL 和 CLOUDFLARE_API_KEY 添加到 DNSEnv 下，并按照指定的格式缩进
+cloudflareEmail=$(yq e ".Nodes[0].ControllerConfig.CertConfig.DNSEnv.CLOUDFLARE_EMAIL" ~/config.yml.bak)
+cloudflareApiKey=$(yq e ".Nodes[0].ControllerConfig.CertConfig.DNSEnv.CLOUDFLARE_API_KEY" ~/config.yml.bak)
+yq eval -i ".Nodes[0].ControllerConfig.CertConfig.DNSEnv.CLOUDFLARE_EMAIL = \"${cloudflareEmail}\"" ~/config.yml.example
+yq eval -i ".Nodes[0].ControllerConfig.CertConfig.DNSEnv.CLOUDFLARE_API_KEY = \"${cloudflareApiKey}\"" ~/config.yml.example
 
-# 移动临时更新文件以替换config.yml.example
-mv ~/temp_updated.yml ~/config.yml.example
-
-# 重命名config.yml.example为config.yml
+# 将更新后的 config.yml.example 重命名为 config.yml
 mv ~/config.yml.example ~/config.yml
 
-# 记录变化到XrayrConfigUpdate.log文件
-cat <<EOL > ~/XrayrConfigUpdate.log
-旧值 ---> 新值
-ApiHost: "$apiHost" ---> ApiHost: "$(yq e '.Nodes[0].ApiConfig.ApiHost' ~/config.yml)"
-EOL
-
-# 清理临时文件
-rm ~/temp_dns_env.yml
+# 创建一个日志文件以记录所做的更改
+echo "旧值 ---> 新值" > ~/XrayrConfigUpdate.log
+for param in PanelType ApiHost ApiKey NodeID NodeType CertMode CertDomain Provider Email; do
+    oldValue=$(yq e ".Nodes[0].${param}" ~/config.yml.bak)
+    newValue=$(yq e ".Nodes[0].${param}" ~/config.yml)
+    echo "${param}: ${oldValue} ---> ${param}: ${newValue}" >> ~/XrayrConfigUpdate.log
+done
