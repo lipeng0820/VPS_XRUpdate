3255a7d1476d07bc15410f46ee2d63e985128c73
lipeng0820 <lipeng0820@qq.com>
2023-10-21 15:04:35 +0800

    Update XRUpdate.sh

--- XRUpdate.sh
+++ XRUpdate.sh
@@ -31,47 +31,55 @@ fi
 mv $CONFIG_PATH $CONFIG_OLD_PATH
 
 # 提取旧配置文件中的特定字段
-NODE_ID=$(grep -Po '^ *NodeID: *\K\d+' $CONFIG_OLD_PATH)
-PANEL_TYPE=$(grep -Po '^ *PanelType: *\K.+' $CONFIG_OLD_PATH)
-API_HOST=$(grep -Po '^ *ApiHost: *\K.+' $CONFIG_OLD_PATH)
-API_KEY=$(grep -Po '^ *ApiKey: *\K.+' $CONFIG_OLD_PATH)
-NODE_TYPE=$(grep -Po '^ *NodeType: *\K.+' $CONFIG_OLD_PATH)
-CERT_MODE=$(grep -Po '^ *CertMode: *\K.+' $CONFIG_OLD_PATH)
-CERT_DOMAIN=$(grep -Po '^ *CertDomain: *\K.+' $CONFIG_OLD_PATH)
-PROVIDER=$(grep -Po '^ *Provider: *\K.+' $CONFIG_OLD_PATH)
-EMAIL=$(grep -Po '^ *Email: *\K.+' $CONFIG_OLD_PATH)
-CF_EMAIL=$(grep -Po '^ *CLOUDFLARE_EMAIL: *\K.+' $CONFIG_OLD_PATH)
-CF_API_KEY=$(grep -Po '^ *CLOUDFLARE_API_KEY: *\K.+' $CONFIG_OLD_PATH)
+extract_value() {
+  grep -Po "^ *$1: *\K.*" $CONFIG_OLD_PATH | sed -e 's/ *#.*//' -e 's/"//g'
+}
+
+NODE_ID=$(extract_value 'NodeID')
+PANEL_TYPE=$(extract_value 'PanelType')
+API_HOST=$(extract_value 'ApiHost')
+API_KEY=$(extract_value 'ApiKey')
+NODE_TYPE=$(extract_value 'NodeType')
+CERT_MODE=$(extract_value 'CertMode')
+CERT_DOMAIN=$(extract_value 'CertDomain')
+PROVIDER=$(extract_value 'Provider')
+EMAIL=$(extract_value 'Email')
+CF_EMAIL=$(extract_value 'CLOUDFLARE_EMAIL')
+CF_API_KEY=$(extract_value 'CLOUDFLARE_API_KEY')
 
 # 修改新配置文件
-sed -i "s/^ *PanelType: .*/ PanelType: $PANEL_TYPE/" ~/config.yml.example
-sed -i "s/^ *ApiHost: .*/ ApiHost: $API_HOST/" ~/config.yml.example
-sed -i "s/^ *ApiKey: .*/ ApiKey: $API_KEY/" ~/config.yml.example
-sed -i "s/^ *NodeID: .*/ NodeID: $NODE_ID/" ~/config.yml.example
-sed -i "s/^ *NodeType: .*/ NodeType: $NODE_TYPE/" ~/config.yml.example
-sed -i "s/^ *CertMode: .*/ CertMode: $CERT_MODE/" ~/config.yml.example
-sed -i "s/^ *CertDomain: .*/ CertDomain: $CERT_DOMAIN/" ~/config.yml.example
-sed -i "s/^ *Provider: .*/ Provider: $PROVIDER/" ~/config.yml.example
-sed -i "s/^ *Email: .*/ Email: $EMAIL/" ~/config.yml.example
-sed -i "/^ *DNSEnv: *$/,/^$/s/^ *CLOUDFLARE_EMAIL: .*/  CLOUDFLARE_EMAIL: $CF_EMAIL/" ~/config.yml.example
-sed -i "/^ *DNSEnv: *$/,/^$/s/^ *CLOUDFLARE_API_KEY: .*/  CLOUDFLARE_API_KEY: $CF_API_KEY/" ~/config.yml.example
+update_config() {
+  sed -i "s|^ *$1: .*|$1: $2|" ~/config.yml.example
+}
+
+update_config 'PanelType' "$PANEL_TYPE"
+update_config 'ApiHost' "$API_HOST"
+update_config 'ApiKey' "$API_KEY"
+update_config 'NodeID' "$NODE_ID"
+update_config 'NodeType' "$NODE_TYPE"
+update_config 'CertMode' "$CERT_MODE"
+update_config 'CertDomain' "$CERT_DOMAIN"
+update_config 'Provider' "$PROVIDER"
+update_config 'Email' "$EMAIL"
+sed -i "/^ *DNSEnv: *$/,/^$/s|^ *CLOUDFLARE_EMAIL: .*|  CLOUDFLARE_EMAIL: $CF_EMAIL|" ~/config.yml.example
+sed -i "/^ *DNSEnv: *$/,/^$/s|^ *CLOUDFLARE_API_KEY: .*|  CLOUDFLARE_API_KEY: $CF_API_KEY|" ~/config.yml.example
 
 # 重命名新配置文件
 mv ~/config.yml.example $CONFIG_PATH
 
 # 记录日志
-echo -e "旧值\t新值" > $LOG_FILE
-echo -e "PanelType: $PANEL_TYPE\tPanelType: $PANEL_TYPE" >> $LOG_FILE
-echo -e "ApiHost: $API_HOST\tApiHost: $API_HOST" >> $LOG_FILE
-echo -e "ApiKey: $API_KEY\tApiKey: $API_KEY" >> $LOG_FILE
-echo -e "NodeID: $NODE_ID\tNodeID: $NODE_ID" >> $LOG_FILE
-echo -e "NodeType: $NODE_TYPE\tNodeType: $NODE_TYPE" >> $LOG_FILE
-echo -e "CertMode: $CERT_MODE\tCertMode: $CERT_MODE" >> $LOG_FILE
-echo -e "CertDomain: $CERT_DOMAIN\tCertDomain: $CERT_DOMAIN" >> $LOG_FILE
-echo -e "Provider: $PROVIDER\tProvider: $PROVIDER" >> $LOG_FILE
-echo -e "Email: $EMAIL\tEmail: $EMAIL" >> $LOG_FILE
-echo -e "CLOUDFLARE_EMAIL: $CF_EMAIL\tCLOUDFLARE_EMAIL: $CF_EMAIL" >> $LOG_FILE
-echo -e "CLOUDFLARE_API_KEY: $CF_API_KEY\tCLOUDFLARE_API_KEY: $CF_API_KEY" >> $LOG_FILE
+echo -e "旧值 --> 新值" > $LOG_FILE
+echo -e "PanelType: $PANEL_TYPE --> PanelType: $PANEL_TYPE" >> $LOG_FILE
+echo -e "ApiHost: $API_HOST --> ApiHost: $API_HOST" >> $LOG_FILE
+echo -e "ApiKey: $API_KEY --> ApiKey: $API_KEY" >> $LOG_FILE
+echo -e "NodeID: $NODE_ID --> NodeID: $NODE_ID" >> $LOG_FILE
+echo -e "NodeType: $NODE_TYPE --> NodeType: $NODE_TYPE" >> $LOG_FILE
+echo -e "CertMode: $CERT_MODE --> CertMode: $CERT_MODE" >> $LOG_FILE
+echo -e "CertDomain: $CERT_DOMAIN --> CertDomain: $CERT_DOMAIN" >> $LOG_FILE
+echo -e "Provider: $PROVIDER --> Provider: $PROVIDER" >> $LOG_FILE
+echo -e "Email: $EMAIL --> Email: $EMAIL" >> $LOG_FILE
+echo -e "CLOUDFLARE_EMAIL: $CF_EMAIL --> CLOUDFLARE_EMAIL: $CF_EMAIL" >> $LOG_FILE
+echo -e "CLOUDFLARE_API_KEY: $CF_API_KEY --> CLOUDFLARE_API_KEY: $CF_API_KEY" >> $LOG_FILE
 
 # 延迟写入数据库
 DELAY=$((NODE_ID * 2))
