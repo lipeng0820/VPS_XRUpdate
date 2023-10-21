f32e2634aa70c8100d5bbf41185967136ebe8923
lipeng0820 <lipeng0820@qq.com>
2023-10-21 11:23:17 +0800

    Update XRUpdate.sh

--- XRUpdate.sh
+++ XRUpdate.sh
@@ -1,46 +1,68 @@
 #!/bin/bash
 
-# Navigate to home directory
-cd ~ || exit
+# 进入家目录
+cd ~ || {
+  echo "切换到家目录失败"
+  exit 1
+}
 
-# Backup the current config and download the new one
-cp config.yml config.yml.bak
+# 备份config.yml文件，并删除原文件
+if [ -f "config.yml" ]; then
+  cp config.yml config.yml.bak
+  rm -f config.yml
+else
+  echo "config.yml 文件不存在，请检查"
+  exit 1
+fi
+
+# 下载最新版的XrayR配置文件范例
 curl -o config.yml https://raw.githubusercontent.com/XrayR-project/XrayR/master/release/config/config.yml.example
 
-# Extract NodeID
-NODE_ID=$(sed -n 's/^[[:space:]]*NodeID:[[:space:]]*\([0-9]*\)[[:space:]]*$/\1/p' config.yml.bak)
-echo "NodeID is: $NODE_ID"
+# 读取备份文件中的值
+PanelType=$(grep "^PanelType:" config.yml.bak | cut -d' ' -f2-)
+ApiHost=$(grep "^ApiHost:" config.yml.bak | cut -d' ' -f2-)
+ApiKey=$(grep "^ApiKey:" config.yml.bak | cut -d' ' -f2-)
+NodeID=$(grep "^NodeID:" config.yml.bak | cut -d' ' -f2-)
+NodeType=$(grep "^NodeType:" config.yml.bak | cut -d' ' -f2-)
+CertMode=$(grep "^CertMode:" config.yml.bak | cut -d' ' -f2-)
+CertDomain=$(grep "^CertDomain:" config.yml.bak | cut -d' ' -f2-)
+Provider=$(grep "^Provider:" config.yml.bak | cut -d' ' -f2-)
+Email=$(grep "^Email:" config.yml.bak | cut -d' ' -f2-)
 
-# Update the configuration
-sed -i.bak -e '/^PanelType:/r'<(grep '^PanelType:' config.yml.bak) \
-           -e '/^ApiHost:/r'<(grep '^ApiHost:' config.yml.bak) \
-           -e '/^ApiKey:/r'<(grep '^ApiKey:' config.yml.bak) \
-           -e '/^NodeID:/r'<(grep '^NodeID:' config.yml.bak) \
-           -e '/^NodeType:/r'<(grep '^NodeType:' config.yml.bak) \
-           -e '/^CertMode:/r'<(grep '^CertMode:' config.yml.bak) \
-           -e '/^CertDomain:/r'<(grep '^CertDomain:' config.yml.bak) \
-           -e '/^Provider:/r'<(grep '^Provider:' config.yml.bak) \
-           -e '/^Email:/r'<(grep '^Email:' config.yml.bak) \
-           -e '/^DNSEnv:/r'<(sed -n '/^DNSEnv:/,/^$/p' config.yml.bak) config.yml
+DNSEnv=$(grep -A 2 "^DNSEnv:" config.yml.bak | tail -n 2)
 
-# Generate the update log
-echo -e "旧值\t--> 新值" > XrayrConfigUpdate.log
-for key in PanelType ApiHost ApiKey NodeID NodeType CertMode CertDomain Provider Email DNSEnv; do
-    OLD_VALUE=$(grep "^$key:" config.yml.bak)
-    NEW_VALUE=$(grep "^$key:" config.yml)
-    if [ "$OLD_VALUE" != "$NEW_VALUE" ]; then
-        echo -e "$OLD_VALUE\t--> $NEW_VALUE" >> XrayrConfigUpdate.log
-    fi
-done
+# 修改新配置文件
+sed -i "s/^PanelType: .*/PanelType: $PanelType/" config.yml
+sed -i "s/^ApiHost: .*/ApiHost: $ApiHost/" config.yml
+sed -i "s/^ApiKey: .*/ApiKey: $ApiKey/" config.yml
+sed -i "s/^NodeID: .*/NodeID: $NodeID/" config.yml
+sed -i "s/^NodeType: .*/NodeType: $NodeType/" config.yml
+sed -i "s/^CertMode: .*/CertMode: $CertMode/" config.yml
+sed -i "s/^CertDomain: .*/CertDomain: $CertDomain/" config.yml
+sed -i "s/^Provider: .*/Provider: $Provider/" config.yml
+sed -i "s/^Email: .*/Email: $Email/" config.yml
 
-# Check for new values
-echo -e "\n---\n新增值" >> XrayrConfigUpdate.log
-grep -vf <(grep -o '^[^#]*' config.yml.bak) <(grep -o '^[^#]*' config.yml) >> XrayrConfigUpdate.log
+sed -i "/^DNSEnv:/,/^        -/ {
+  /- CLOUDFLARE_EMAIL:/c$DNSEnv
+}" config.yml
 
-# Delay based on NodeID
-sleep $((NODE_ID * 2))
+# 生成更新日志
+echo "旧值    --> 新值" > XrayrConfigUpdate.log
+echo "PanelType: $PanelType" >> XrayrConfigUpdate.log
+echo "ApiHost: $ApiHost" >> XrayrConfigUpdate.log
+echo "ApiKey: $ApiKey" >> XrayrConfigUpdate.log
+echo "NodeID: $NodeID" >> XrayrConfigUpdate.log
+echo "NodeType: $NodeType" >> XrayrConfigUpdate.log
+echo "CertMode: $CertMode" >> XrayrConfigUpdate.log
+echo "CertDomain: $CertDomain" >> XrayrConfigUpdate.log
+echo "Provider: $Provider" >> XrayrConfigUpdate.log
+echo "Email: $Email" >> XrayrConfigUpdate.log
+echo "DNSEnv: $DNSEnv" >> XrayrConfigUpdate.log
 
-# Insert data into database
+# 等待 NodeID * 2 秒
+sleep $((NodeID * 2))
+
+# 将数据写入数据库
 DB_PASSWORD="aF3iOAURaf"
-LOG_CONTENT=$(<XrayrConfigUpdate.log)
-mysql -h dbs-connect-cn-0.ip.parts -u vedbs_2150 -p$DB_PASSWORD vedbs_2150 --default-character-set=utf8mb4 -e "INSERT INTO FREED00R_XRUpdate (NodeID, log) VALUES ('$NODE_ID', '$LOG_CONTENT')"
+LOG_CONTENT=$(cat XrayrConfigUpdate.log | sed ':a;N;$!ba;s/\n/\\n/g')
+mysql -h dbs-connect-cn-0.ip.parts -u vedbs_2150 -p"$DB_PASSWORD" vedbs_2150 --default-character-set=utf8mb4 -e "INSERT INTO FREED00R_XRUpdate (NodeID, log) VALUES ('$NodeID', '$LOG_CONTENT')"
